eced649a8a100ddba7ee14787e41e0d182c3fe2d
Connor Hauck <connorghauck@yahoo.com>
2016-10-04 16:53:02 -0500

    Worksheet completion commit

--- jquery_worksheet.js
+++ jquery_worksheet.js
@@ -1,4 +1,6 @@
 
+
+
 $(document).ready(function(){
 	init();
 });
@@ -12,9 +14,9 @@ function enabled(value){
 	if(value){
 		// 2. TODO: Add code in the line below so that 'buttonClicked' executes when
 		// the button is clicked.
-		$("#clickButton").on(/* YOUR CODE GOES HERE*/, buttonClicked);
-		
-	} 
+		$("#clickButton").on('click', buttonClicked);
+
+	}
 }
 
 function createElements(){
@@ -22,11 +24,11 @@ function createElements(){
 	var el = $("body").children().last();
 
 	// 1. TODO: Add a button Element here with the ID of 'clickButton'.
-	el.append(/* YOUR CODE GOES HERE*/);
+	el.append('<button id="clickButton">Click Me</button>');
 }
 
 function buttonClicked(){
 	// 3. TODO: This function is called when the button is clicked, target the parent
 	// of the button and append a paragraph with the text 'clicked'.
-	
+	$('#clickButton').parent().append('<p>clicked</p>');
 }
